81f2a2e196bb913e3fd255fe52dd799ff061d75f
viz06 <68382946+viz06@users.noreply.github.com>
2020-09-15 15:18:07 +0530

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,29 @@
+'''x,y=input("enter 2 values:").split()
+print("first{} and secnd{}".format(x,y));'''
+'''x=list(map(int,input("enteer multiple values: ").split()))
+print("values are: ",x)'''
+'''x,y=[int(x) for x in input("enter 2 values: ").split()]
+print("first {} and second {}".format(x,y))'''
+'''x=[int(x) for x in input("enter multiple values: ").split()]
+print("values are :",x)'''
+'''x=[int(x) for x in input("enter multiple values: ").split()]
+print("values are: ",x)'''
+'''print("hello", end=" "),
+print("viz")
+a=[1,2,3,4]
+for i in range(4):
+    print(a[i],end=" ")'''
+'''print("happy",end=" ")
+print("bithday")
+print('happy','birthday', sep=' ',end="@")
+print("madhav")'''
+'''tab={'viz':1, 'motti':2, 'red':3}
+print('{0[viz]:d},{0[motti]:d},{0[red]:d}'.format(tab))'''
+
+'''line="-"
+name=dict(viz="grover",motti="gaushala")
+print('{viz},{motti}'.format(**name))
+print(line.center(40,'-'))
+print("i want {1} and {0}".format('oreo','milk'))'''
+
+
