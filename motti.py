59057a05d853a891a70a6b7cd9a3875a92b6256a
viz06 <68382946+viz06@users.noreply.github.com>
2020-09-15 15:18:29 +0530

    Add files via upload

---
+++ motti.py
@@ -0,0 +1,52 @@
+'''a=244
+print ("a")
+a=int(input(""))
+print (a)
+a=int(input(""))
+b=int(input(""))
+print("the sum of two no's is",a+b)
+print("the subtraction of two no's is",a-b)
+print("the division of two no's is",a/b)
+print("the multiplication of two no's is",a*b)'''
+'''a=int(input(""))
+b=int(input(""))
+c=2*(a+b)
+d=a*b
+print("the perimeter is",c)
+print("the area is",d)'''
+'''r=int(input(""))
+d=2*r
+c=2*3.14*r
+a=3.14*r*r
+print("the diameter of circle is",d)
+print("the circumfrence of circle is",c)
+print("the area of circle is",a)'''
+'''cm=int(input(""))
+km=cm*1000
+m=cm*100
+print("In km",km)
+print("in m",m)'''
+'''a=int(input(""))
+b=int(input(""))
+if(a>b):
+    print (a)
+else:
+    print (b)'''
+'''a=int(input(""))
+b=int(input(""))
+c=int(input(""))
+if(a>b):
+    if(a>c):
+        print(a)
+    else:
+        print(c)
+if(b>a):
+    if(b>c):
+        print(b)
+    else:
+        print(c)'''
+a=int(input(""))
+if(a%2==0):
+    print(a,"is even")
+else:
+    print(a,"is odd")
